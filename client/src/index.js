d2f91b61ad6fac0bb283260105278fc3b0f99ca1
Walter <walter.clark24@yahoo.com>
2023-03-20 15:37:53 -0400

    add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import "semantic-ui-css/semantic.min.css";
 import {BrowserRouter as Router} from "react-router-dom"
 // import Userfront from "@userfront/core";
-
+import "..node/modules_react-bootstrap/dist/react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <Router>
       <App />
+      
     </Router>
   // {/* </React.StrictMode> */}
 );
